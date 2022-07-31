e3732f89e2dd9d5225582db6afb50d684032e0e3
Wojciech Malarski <w.malarski@gmail.com>
2022-07-31 13:32:51 +0200

    solved easy issues

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,5 @@
-type MyAwaited<T> = any
+type MyAwaitedAux<T> = T extends Promise<infer A> ? MyAwaitedAux<A> : T
+
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer A>
+  ? MyAwaitedAux<A>
+  : never
