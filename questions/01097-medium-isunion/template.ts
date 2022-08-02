56462d0edaa3f408106f4416cc66c9637947f4f4
Wojciech Malarski <w.malarski@gmail.com>
2022-08-02 19:11:59 +0200

    is union solver

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,5 +1,7 @@
-type IsUnion<T, S = T> = [S] extends [never]
-  ? T extends S
-    ? false
-    : true
-  : true
+type IsUnion<T, T2 = T> = [T] extends [never]
+  ? false
+  : T extends T2
+    ? [T2] extends [T]
+        ? false
+        : true
+    : false
